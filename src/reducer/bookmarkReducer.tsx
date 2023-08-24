d7bd13ffbe148b3b35df13938e38f2b24484111e
Rockey Biswas <85108386+ncrptd@users.noreply.github.com>
2023-08-24 15:44:00 +0530

    initial commit

--- src/reducer/bookmarkReducer.tsx
+++ src/reducer/bookmarkReducer.tsx
@@ -1,17 +1,22 @@
-import { v4 as uuidv4 } from "uuid";
-import { BookmarksAction, BookmarksState } from "../context/BookmarkContext";
+import { v4 as uuidv4 } from 'uuid';
+import { BookmarksAction, BookmarksState } from '../context/BookmarkContext';
 
 const bookmarkReducer = (state: BookmarksState, action: BookmarksAction) => {
-  const { type, payload } = action;
-  switch (type) {
-    case "ADD_BOOKMARK": {
-      const { title, url } = payload;
+  switch (action.type) {
+    case 'ADD_BOOKMARK': {
       return {
         ...state,
-        bookmarks: [...state.bookmarks, { title, url, id: uuidv4() }]
+        bookmarks: [
+          ...state.bookmarks,
+          {
+            title: action.payload.title,
+            url: action.payload.url,
+            id: uuidv4(),
+          },
+        ],
       };
     }
-    case "REMOVE_BOOKMARK": {
+    case 'REMOVE_BOOKMARK': {
       const id = action.payload;
       const updatedBookmarks = state.bookmarks.filter(
         (bookmark) => id !== bookmark.id
